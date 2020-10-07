dc8b2fda6bd66619537eecd198c2db0079a679f8
Sanvi <69363056+SanviSegireddy@users.noreply.github.com>
2020-10-07 17:29:08 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,16 @@
+class Box extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+     
+    }
+    block1= new block(330,235,30,40);
+block2= new block(360,235,30,40);
+block3= new block(390,235,30,40);
+block4= new block(420,235,30,40);
+block5= new block(450,235,30,40);
+block6= new block(360,195,30,40);
+block7= new block(390,195,30,40);
+block8= new block(420,195,30,40);
+block9= new block(390,155,30,40);
+  
+  };
